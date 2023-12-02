660b4699216b84cc5b0961ce2e71bd240bca846a
T-126 <rathnayaka3548@gmail.com>
2023-12-02 17:20:03 +0530

    Main files added

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Home from "./components/dec02/Home";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
---
+++ src/components/dec02/Home.jsx
@@ -0,0 +1,28 @@
+import "./Home.css";
+import "./Root.css";
+import "./Button.css";
+import "./Span.css";
+import "./Anchor.css";
+
+export default function Home() {
+  return (
+    <div id="home">
+      <div className="container">
+        <button className="t-btn">Button</button> &nbsp;&nbsp;
+        <button className="t-btn t-btn-primary">Button-Primary</button>
+      </div>
+
+      <div className="container">
+        Hey, yeah, I'm the <span className="highlight-text">BEST</span>!
+      </div>
+
+      <div className="container">
+        Here's my{" "}
+        <a className="highlight-link" href="https://github.com/Thili-126">
+          GitHub link
+        </a>
+      </div>
+    </div>
+  );
+}
+w;
