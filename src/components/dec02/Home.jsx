045bec16342a5043acd7e770551f2f6b257d982c
T-126 <rathnayaka3548@gmail.com>
2023-12-02 19:30:02 +0530

    Navbar added

--- src/components/dec02/Home.jsx
+++ src/components/dec02/Home.jsx
@@ -3,25 +3,28 @@ import "./Root.css";
 import "./Button.css";
 import "./Span.css";
 import "./Anchor.css";
+import Navbar from "./Navber";
 
 export default function Home() {
   return (
-    <div id="home">
-      <div className="container">
-        <button className="t-btn">Button</button> &nbsp;&nbsp;
-        <button className="t-btn t-btn-primary">Button-Primary</button>
-      </div>
+    <>
+      <div id="home">
+        <div className="container">
+          <button className="t-btn">Button</button> &nbsp;&nbsp;
+          <button className="t-btn t-btn-primary">Button-Primary</button>
+        </div>
 
-      <div className="container">
-        Hey, yeah, I'm the <span className="highlight-text">BEST</span>!
-      </div>
+        <div className="container">
+          Hey, yeah, I'm the <span className="highlight-text">BEST</span>!
+        </div>
 
-      <div className="container">
-        Here's my{" "}
-        <a className="highlight-link" href="https://github.com/Thili-126">
-          GitHub link
-        </a>
+        <div className="container">
+          Here's my{" "}
+          <a className="highlight-link" href="https://github.com/Thili-126">
+            GitHub link
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
