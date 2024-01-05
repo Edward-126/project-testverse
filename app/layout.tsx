1f3b76d67355183d17718ee819360da067f8d01c
T-126 <rathnayaka3548@gmail.com>
2024-01-05 21:22:14 +0530

    chore: base added + cleanup

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Cabin } from "next/font/google";
+import "./globals.css";
+
+const cabin = Cabin({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "TestVerse",
+  description: "My Testing Ground",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cabin.className}>{children}</body>
+    </html>
+  );
+}
